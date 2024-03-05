6a006801840f8307abd120c2fb3ee5def7fffb74
tanglei <tlei7971@163.com>
2024-03-05 08:45:22 +0000

    !124 清除脚本多余打屏 * 丢弃脚本多余打屏

--- testcases/test_model.sh
+++ testcases/test_model.sh
@@ -18,22 +18,22 @@ fi
 if [[ "$param" == "all" || "$param" == "hccl-test" || "$param" == "ais-flops" || "$param" == "flops-test" || "$param" == "distributed" ]]; then
     # 如果没有参数或者参数为all，则依次执行所有脚本
     if [[ "$param" == "all" ]]; then
-        bash "$hccl_dir/hccl_test.sh"
-        bash "$flops_dir/flops_test.sh"
-        bash "$distributed_dir/server_train.sh"
-        bash "$ais_dir/ais_bench.sh"
+        bash "$hccl_dir/hccl_test.sh" >/dev/null 2>&1
+        bash "$flops_dir/flops_test.sh" >/dev/null 2>&1
+        bash "$distributed_dir/server_train.sh" >/dev/null 2>&1
+        bash "$ais_dir/ais_bench.sh" >/dev/null 2>&1
     # 如果参数为hccl-test，则执行hccl_test.sh脚本
     elif [[ "$param" == "hccl-test" ]]; then
-        bash "$hccl_dir/hccl_test.sh" &
+        bash "$hccl_dir/hccl_test.sh" >/dev/null 2>&1 &
     # 如果参数为ais-flops，则执行ais_bench.sh脚本
     elif [[ "$param" == "ais-flops" ]]; then
-        bash "$ais_dir/ais_bench.sh" &
+        bash "$ais_dir/ais_bench.sh" >/dev/null 2>&1 &
     # 如果参数为flops-test，则执行flops_test.sh脚本
     elif [[ "$param" == "flops-test" ]]; then
-        bash "$flops_dir/flops_test.sh" &
+        bash "$flops_dir/flops_test.sh" >/dev/null 2>&1 &
     # 如果参数为distributed，则执行server_train.sh脚本
     elif [[ "$param" == "distributed" ]]; then
-        bash "$distributed_dir/server_train.sh" &
+        bash "$distributed_dir/server_train.sh" >/dev/null 2>&1 &
     else
         echo "Invalid parameter."
         exit 1
