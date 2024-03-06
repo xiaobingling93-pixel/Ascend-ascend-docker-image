6ad9944746aa0f4ea85cd3e30b51d59308ffbb1b
tanglei <tlei7971@163.com>
2024-03-06 08:23:57 +0000

    !131 fix统一镜像测试脚本输入参数错误导致终端退出 * fix统一镜像测试脚本输入参数错误导致终端退出

--- testcases/test_model.sh
+++ testcases/test_model.sh
@@ -9,37 +9,27 @@ flops_dir="flops"
 ais_dir="ais"
 distributed_dir="distributed"
 
-if [ "$#" -gt 1 ]; then
-    echo "Too many parameters。"
-    exit 1
-fi
-
-# 检查参数是否为预期值
-if [[ "$param" == "all" || "$param" == "hccl-test" || "$param" == "ais-flops" || "$param" == "flops-test" || "$param" == "distributed" ]]; then
-    # 如果没有参数或者参数为all，则依次执行所有脚本
-    if [[ "$param" == "all" ]]; then
-        bash "$hccl_dir/hccl_test.sh" >/dev/null 2>&1
-        bash "$flops_dir/flops_test.sh" >/dev/null 2>&1
-        bash "$distributed_dir/server_train.sh" >/dev/null 2>&1
-        bash "$ais_dir/ais_bench.sh" >/dev/null 2>&1
+if [ "$#" -eq 1 ]; then
+  if [[ "$param" == "all" ]]; then
+    bash "$hccl_dir/hccl_test.sh" >/dev/null 2>&1
+    bash "$flops_dir/flops_test.sh" >/dev/null 2>&1
+    bash "$distributed_dir/server_train.sh" >/dev/null 2>&1
+    bash "$ais_dir/ais_bench.sh" >/dev/null 2>&1
     # 如果参数为hccl-test，则执行hccl_test.sh脚本
     elif [[ "$param" == "hccl-test" ]]; then
-        bash "$hccl_dir/hccl_test.sh" >/dev/null 2>&1 &
+      bash "$hccl_dir/hccl_test.sh" >/dev/null 2>&1 &
     # 如果参数为ais-flops，则执行ais_bench.sh脚本
     elif [[ "$param" == "ais-flops" ]]; then
-        bash "$ais_dir/ais_bench.sh" >/dev/null 2>&1 &
+      bash "$ais_dir/ais_bench.sh" >/dev/null 2>&1 &
     # 如果参数为flops-test，则执行flops_test.sh脚本
     elif [[ "$param" == "flops-test" ]]; then
-        bash "$flops_dir/flops_test.sh" >/dev/null 2>&1 &
+      bash "$flops_dir/flops_test.sh" >/dev/null 2>&1 &
     # 如果参数为distributed，则执行server_train.sh脚本
     elif [[ "$param" == "distributed" ]]; then
-        bash "$distributed_dir/server_train.sh" >/dev/null 2>&1 &
+      bash "$distributed_dir/server_train.sh" >/dev/null 2>&1 &
     else
-        echo "Invalid parameter."
-        exit 1
-    fi
+      echo "Invalid parameter."
+  fi
 else
-    echo "Invalid parameter."
-    exit 1
-
+    echo "Too many parameters。"
 fi
