4c202d41a57e9c2d44b86f1d7647fc7b4b5b0789
Jeaniowang <1104133197@qq.com>
2025-05-15 17:15:22 +0800

    添加tei构建脚本

--- mis/build_image.sh
+++ mis/build_image.sh
@@ -7,9 +7,13 @@ workdir=$(
 
 function build_cann_image() {
   cann_image=$2
-
+  python_version=$3
   if [ -z "$cann_image" ]; then
     cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$python_version" ]; then
+    python_version="3.10
   fi
 
   echo "build cann image with name [$cann_image]"
@@ -19,7 +23,7 @@ function build_cann_image() {
   else
     echo "building cann image: ${cann_image}"
     cd "$workdir"/dockerfiles/cann || exit
-    docker build -t $cann_image . || exit
+    docker build -t --build-arg PYTHON_VER=${python_version} $cann_image . || exit
   fi
 }
 
@@ -42,6 +46,74 @@ function build_llm_base_image() {
     echo "building llm-base image: ${llm_base_image}"
     cd "$workdir"/dockerfiles/llm/base || exit
     docker build --build-arg BASE_IMAGE=$cann_image -t $llm_base_image . || exit
+  fi
+}
+
+function build_tei_base_image() {
+  cann_image=$2
+  tei_base_image=$3
+  platform=$4
+
+  if [ -z "$cann_image" ]; then
+    cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$tei_base_image" ]; then
+    tei_base_image="mis-tei-base:0.1"
+  fi
+
+  if [ -z "$platform" ]; then
+    platform="910B"
+  fi
+
+  echo "from [$cann_image] build llm-base image with name [$tei_base_image]"
+
+  CD $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/emb/tei/base/build
+  mkdir -p $docker_build_dir
+  cp -r $workdir/patch/tei.patch $docker_build_dir
+  cd $workdir/dockerfiles/emb/base || exit
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${tei_base_image}&"; then
+    echo "llm-base image: ${tei_base_image} exist"
+  else
+    echo "building llm-base image: ${tei_base_image}"
+    cd "$workdir"/dockerfiles/llm/base || exit
+    docker build --build-arg BASE_IMAGE=$cann_image --build-arg ARCH=$(uname -m) --build-arg PLATFORM=${platform} -t $tei_base_image . || exit
+  fi
+}
+
+function build_clip_base_image() {
+  cann_image=$2
+  clip_base_image=$3
+  platform=$4
+
+  if [ -z "$cann_image" ]; then
+    cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$clip_base_image" ]; then
+    clip_base_image="mis-clip-base:0.1"
+  fi
+
+  if [ -z "$platform" ]; then
+    platform="910B"
+  fi
+
+  echo "from [$cann_image] build llm-base image with name [$clip_base_image]"
+
+  CD $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/emb/tei/base/build
+  mkdir -p $docker_build_dir
+  cp -r $workdir/patch/tei.patch $docker_build_dir
+  cd $workdir/dockerfiles/emb/base || exit
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${clip_base_image}&"; then
+    echo "llm-base image: ${clip_base_image} exist"
+  else
+    echo "building llm-base image: ${clip_base_image}"
+    cd "$workdir"/dockerfiles/llm/base || exit
+    docker build --build-arg BASE_IMAGE=$cann_image --build-arg ARCH=$(uname -m) --build-arg PLATFORM=${platform} -t $clip_base_image . || exit
   fi
 }
 
@@ -81,16 +153,109 @@ function build_model_image() {
   docker build --build-arg BASE_IMAGE=$llm_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
 }
 
+
+function build_tei_model_image() {
+  tei_base_image=$2
+  model_name=$3
+  version=$4
+
+  if [ -z "$tei_base_image" ]; then
+    tei_base_image="mis-tei-base:0.1"
+  fi
+
+  if [ -z "$model_name" ]; then
+    model_name="bge-large-zh-v1.5"
+  fi
+
+  if [ -z "$version" ]; then
+    version="0.1"
+  fi
+
+  typeset -l model_name_lower
+  model_name_lower=$model_name
+
+  echo "from [$tei_base_image] build model image with name [$model_name_lower:$version]"
+
+  cd $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/emb/tei/model/build
+  mkdir -p "$docker_build_dir"
+
+  if [[ ! -e $workdir/configs/emb/tei/$model_name/config.json ]]; then
+      echo "$workdir/configs/emb/tei/$model_name/config.json not exist, build image failed"
+      exit 1
+  fi
+
+  cp -r $workdir/configs/emb/tei/${model_name}/config.json $docker_build_dir
+  cp -r $workdir/mis $docker_build_dir
+
+  cd $workdir/dockerfiles/emb/tei/model || exit
+
+  docker build --build-arg BASE_IMAGE=$tei_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
+}
+
+
+
+function build_clip_model_image() {
+  clip_base_image=$2
+  model_name=$3
+  version=$4
+
+  if [ -z "$clip_base_image" ]; then
+    clip_base_image="mis-clip-base:0.1"
+  fi
+
+  if [ -z "$model_name" ]; then
+    model_name="ViT-B-16"
+  fi
+
+  if [ -z "$version" ]; then
+    version="0.1"
+  fi
+
+  typeset -l model_name_lower
+  model_name_lower=$model_name
+
+  echo "from [$clip_base_image] build model image with name [$model_name_lower:$version]"
+
+  cd $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/emb/clip/model/build
+  mkdir -p "$docker_build_dir"
+
+  if [[ ! -e $workdir/configs/emb/clip/$model_name/config.yaml ]]; then
+      echo "$workdir/configs/emb/clip/$model_name/config.yaml not exist, build image failed"
+      exit 1
+  fi
+
+  cp -r $workdir/configs/emb/clip/${model_name}/config.yaml $docker_build_dir
+  cp -r $workdir/mis $docker_build_dir
+
+  cd $workdir/dockerfiles/emb/clip/model || exit
+  sed -i "s|export MIS_MODEL=.*|export MIS_MODEL=MindSDK/${model_name}|g" $workdir/mis/start_clip.sh
+
+  docker build --build-arg BASE_IMAGE=$clip_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
+}
+
 stage=$1
 case $stage in
   "cann")
-    build_cann_image "$1" "$2"
+    build_cann_image "$@"
     ;;
   "llm-base")
-    build_llm_base_image "$1" "$2" "$3"
+    build_llm_base_image "$@"
     ;;
-  "model")
-    build_model_image "$1" "$2" "$3" "$4"
+  "tei-base")
+    build_tei_base_image "$@"
     ;;
-  *) echo "Invalid stage, we only support [cann, llm-base, model]"
+  "clip-base")
+    build_clip_base_image "$@"
+    ;;
+  "llm-model")
+    build_model_image "$@"
+    ;;
+  "tei-model")
+    build_tei_model_image "$@"
+    ;;
+  "clip-model")
+    build_clip_model_image "$@"
+  *) echo "Invalid stage, we only support [cann, llm-base, tei-base, clip-base, llm-model, tei-model, clip-model]"
 esac
---
+++ mis/dockerfiles/clip/model/start_clip.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Copyright © Huawei Technologies Co., Ltd. 2024. All rights reserved.
+CUR_PATH=$(dirname "$(readlink -f "$0")")
+ROOT_PATH=$(readlink -f "$CUR_PATH")
+
+if [[ "$#" -ne 2 ]]; then
+    echo "Need param:<listen_ip> <listen_port>"
+    exit 1
+fi
+
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+
+export MIS_ENGINE_TYPE="clip-service"
+export CLIP_CONFIG_PATH=$CUR_PATH/config.yaml
+export MIS_CACHE_PATH="/opt/mis/.cache"
+export MIS_MODEL=MindSDK/ViT-B-16
+
+export JINA_HIDE_SURVEY=1
+
+export MIS_HOST=$1
+export MIS_PORT=$2
+export no_proxy=127.0.0.1,localhost
+
+# inner server port
+if [[ -z $INNER_PORT ]]; then
+	export INNER_PORT=9090
+fi
+
+#modify inner port
+sed -i "s|2025|$INNER_PORT|g" "$CUR_PATH"/config.yaml
+
+
+function check_model_exists() {
+	ret=$(ls ${MIS_CACHE_PATH}/${MIS_MODEL}/*.pt | wc -l)
+
+    if [[ $ret -eq 0 ]]; then
+        echo "Model '${MIS_CACHE_PATH}/${MIS_MODEL}' does not exist."
+        return 1
+    else
+        echo "Model '${MIS_CACHE_PATH}/${MIS_MODEL}' exists."
+        return 0
+    fi
+}
+
+function download_model() {
+    local retry_time=1
+    local max_retries=5
+    echo "Downloading model '${MIS_MODEL}' from modelscope ..."
+    while [[ ${retry_time} -le ${max_retries} ]]; do
+		rm -rf "${MIS_CACHE_PATH}/${MIS_MODEL}"
+		mkdir -p "${MIS_CACHE_PATH}/MindSDK"
+
+        if cd "${MIS_CACHE_PATH}/MindSDK" && git clone "https://modelers.cn/${MIS_MODEL}.git" "${MIS_MODEL##*/}" && cd "${MIS_MODEL##*/}" && git lfs pull; then
+            echo "Download successful."
+            return 0
+        else
+            retry_time=$((retry_time + 1))
+            echo "Download failed, try again."
+            sleep 5
+        fi
+    done
+    echo "Maximum retries ${max_retries} reached. Download failed."
+    return 1
+}
+
+if ! check_model_exists; then
+	if ! download_model; then
+		echo "Download model $MIS_MODEL failed" 
+		exit 1
+	fi
+fi
+
+mis_clip
