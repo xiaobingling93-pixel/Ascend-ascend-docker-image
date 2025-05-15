960cc2491164f24497726a7bad6fd576a86fdec5
damttt <yeyuming5@huawei.com>
2025-05-15 03:45:21 +0000

    !151 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本 * 【修改说明 Modification】调整mis镜像构建脚本

--- mis/README.md
+++ mis/README.md
@@ -1,6 +1,8 @@
 # MIS(MindSDK Inference Server)
 
-## 版本说明
+## llm镜像制作
+
+### 版本说明
 
 - 支持硬件 昇腾Atlas 800I A2
 
@@ -12,12 +14,13 @@
 | vLLM        | 推理框架         | [0.7.1](https://github.com/vllm-project/vllm/tree/v0.7.1)                                                                        |
 | vllm-ascend | 昇腾vllm框架支持   | [0.7.1rc1](https://github.com/vllm-project/vllm-ascend/tree/v0.7.1rc1) |
 
-## 构建说明
+### 构建说明
 
 构建镜像前，需要准备mis发布包，并解压至当前目录
 ```text
 configs/
-mis-{version}-cp310-cp310-linux_aarch64.whl
+vlm/
+llm/
 patch/
 ```
 还需要准备cann，mindie相关软件包，并放置于任意目录
@@ -28,7 +31,64 @@ Ascend-cann-nnal_8.0.0_linux-aarch64.run
 Ascend-mindie_1.0.0_linux-aarch64.run
 Ascend-mindie-atb-models_1.0.0_linux-aarch64_py310_torch2.3.1-abi0.tar.gz
 ```
-准备server.js文件，和上述软件包放置于相同目录
+准备server.js文件，和上述软件包放置于相同目录，server.js文件见附录
+使用以下命令启动服务，此时构建镜像可以通过http的方式下载软件包，减少镜像的大小，nodejs版本无要求
+```shell
+nodejs server.js
+```
+回到MIS目录，參考以下命令完成镜像构建
+```shell
+bash build_image.sh llm-cann mis-llm-cann:0.1
+bash build_image.sh llm-base mis-llm-cann:0.1 mis-llm-base:0.1
+bash build_image.sh llm-model mis-llm-base:0.1 DeepSeek-R1-Distill-Qwen-7B 0.1
+```
+
+## vlm镜像制作
+
+### 版本说明
+
+- 支持硬件 昇腾Atlas 800I A2
+
+| 组件          | 类型           | 版本                                                                                                                                     |
+|-------------|--------------|----------------------------------------------------------------------------------------------------------------------------------------|
+| Ubuntu      | 系统镜像         | 22.04                                                                                                                                  |
+| cann        | 昇腾异构计算架构和算子库 | [8.1.RC1.beta1](https://www.hiascend.com/developer/download/community/result?module=cann&cann=8.1.RC1.beta1)                           |
+| mindie      | 昇腾推理引擎       | [2.0.RC1](https://www.hiascend.com/developer/download/community/result?module=ie+pt+cann&ie=2.0.RC1&pt=7.0.0.beta1&cann=8.1.RC1.beta1) |
+| vLLM        | 推理框架         | [0.8.5.post1](https://github.com/vllm-project/vllm/tree/v0.8.5.post1)                                                                  |
+| vllm-ascend | 昇腾vllm框架支持   | [0.8.5rc1](https://github.com/vllm-project/vllm-ascend/tree/v0.8.5rc1)                                                                 |
+
+### 构建说明
+
+构建镜像前，需要准备mis发布包，并解压至当前目录
+```text
+configs/
+llm/
+vlm/
+patch/
+```
+还需要准备cann，mindie相关软件包，并放置于任意目录
+```text
+Ascend-cann-toolkit_8.1.RC1_linux-aarch64.run
+Ascend-cann-kernels-910b_8.1.RC1_linux-aarch64.run
+Ascend-cann-nnal_8.1.RC1_linux-aarch64.run
+Ascend-mindie_2.0.RC1_linux-aarch64.run
+Ascend-mindie-atb-models_2.0.RC1_linux-aarch64_py310_torch2.3.1-abi0.tar.gz
+```
+准备server.js文件，和上述软件包放置于相同目录，server.js文件见附录
+使用以下命令启动服务，此时构建镜像可以通过http的方式下载软件包，减少镜像的大小，nodejs版本无要求
+```shell
+nodejs server.js
+```
+回到MIS目录，參考以下命令完成镜像构建
+```shell
+bash build_image.sh vlm-cann mis-vlm-cann:0.1
+bash build_image.sh vlm-base mis-vlm-cann:0.1 mis-vlm-base:0.1
+bash build_image.sh vlm-model mis-vlm-base:0.1 Qwen2.5-VL-7B-Instruct 0.1
+```
+
+## 附录
+
+### server.js
 ```js
 const http = require('http');
 const fs = require('fs');
@@ -72,14 +132,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
 });
-```
-使用以下命令启动服务，此时构建镜像可以通过http的方式下载软件包，减少镜像的大小，nodejs版本无要求
-```shell
-nodejs server.js
-```
-回到MIS目录，參考以下命令完成镜像构建
-```shell
-bash build_image.sh cann mis-cann:0.1
-bash build_image.sh llm-base mis-cann:0.1 mis-llm-base:0.1
-bash build_image.sh model mis-llm-base:0.1 DeepSeek-R1-Distill-Qwen-7B 0.1
 ```
--- mis/build_image.sh
+++ mis/build_image.sh
@@ -5,92 +5,189 @@ workdir=$(
   pwd
 )
 
-function build_cann_image() {
-  cann_image=$2
+default_llm_cann=mis-llm-cann:0.1
+default_vlm_cann=mis-vlm-cann:0.1
+default_llm_base=mis-llm-base:0.1
+default_vlm_base=mis-vlm-base:0.1
+default_llm_model=DeepSeek-R1-Distill-Qwen-7B
+default_vlm_model=Qwen2.5-VL-7B-Instruct
+default_version=0.1
 
-  if [ -z "$cann_image" ]; then
-    cann_image="mis-cann:0.1"
+
+function build_llm_cann_image() {
+  llm_cann_image=$2
+
+  if [ -z "${llm_cann_image}" ]; then
+    llm_cann_image=${default_llm_cann}
   fi
 
-  echo "build cann image with name [$cann_image]"
+  echo "build llm cann image with name [${llm_cann_image}]"
 
-  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${cann_image}&"; then
-    echo "cann image: ${cann_image} exist"
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${llm_cann_image}&"; then
+    echo "building llm cann image: ${llm_cann_image} exist"
   else
-    echo "building cann image: ${cann_image}"
+    echo "building llm cann image: ${llm_cann_image}"
     cd "$workdir"/dockerfiles/cann || exit
-    docker build -t $cann_image . || exit
+    docker build -t ${llm_cann_image} -f Dockerfile_llm . || exit
+  fi
+}
+
+function build_vlm_cann_image() {
+  vlm_cann_image=$2
+
+  if [ -z "${vlm_cann_image}" ]; then
+    vlm_cann_image=$default_vlm_base
+  fi
+
+  echo "build vlm cann image with name [${vlm_cann_image}]"
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${vlm_cann_image}&"; then
+    echo "building vlm cann image: ${vlm_cann_image} exist"
+  else
+    echo "building vlm cann image: ${vlm_cann_image}"
+    cd "$workdir"/dockerfiles/cann || exit
+    docker build -t ${vlm_cann_image} -f Dockerfile_vlm . || exit
   fi
 }
 
 function build_llm_base_image() {
-  cann_image=$2
-  if [ -z "$cann_image" ]; then
-    cann_image="mis-cann:0.1"
+  llm_cann_image=$2
+  if [ -z "${llm_cann_image}" ]; then
+    llm_cann_image=${default_llm_cann}
   fi
 
   llm_base_image=$3
-  if [ -z "$llm_base_image" ]; then
-    llm_base_image="mis-llm-base:0.1"
+  if [ -z "${llm_base_image}" ]; then
+    llm_base_image=$default_llm_base
   fi
 
-  echo "from [$cann_image] build llm-base image with name [$llm_base_image]"
+  echo "from [${llm_cann_image}] build llm-base image with name [${llm_base_image}]"
 
   if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${llm_base_image}&"; then
-    echo "llm-base image: ${llm_base_image} exist"
+    echo "building llm-base image: ${llm_base_image} exist"
   else
     echo "building llm-base image: ${llm_base_image}"
     cd "$workdir"/dockerfiles/llm/base || exit
-    docker build --build-arg BASE_IMAGE=$cann_image -t $llm_base_image . || exit
+    docker build --build-arg BASE_IMAGE=${llm_cann_image} -t ${llm_base_image} . || exit
   fi
 }
 
-function build_model_image() {
+function build_vlm_base_image() {
+  vlm_cann_image=$2
+  if [ -z "${vlm_cann_image}" ]; then
+    vlm_cann_image=${default_vlm_cann}
+  fi
+
+  vlm_base_image=$3
+  if [ -z "${vlm_base_image}" ]; then
+    vlm_base_image=${default_vlm_base}
+  fi
+
+  echo "from [${vlm_cann_image}] build llm-base image with name [${vlm_base_image}]"
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${vlm_base_image}&"; then
+    echo "building vlm-base image: ${vlm_base_image} exist"
+  else
+    echo "building vlm-base image: ${vlm_base_image}"
+    cd "$workdir"/dockerfiles/vlm/base || exit
+    docker build --build-arg BASE_IMAGE=${vlm_cann_image} -t ${vlm_base_image} . || exit
+  fi
+}
+
+function build_llm_model_image() {
   llm_base_image=$2
   model_name=$3
   version=$4
 
-  if [ -z "$llm_base_image" ]; then
-    llm_base_image="mis-llm-base:0.1"
+  if [ -z "${llm_base_image}" ]; then
+    llm_base_image=${default_llm_base}
   fi
 
   if [ -z "$model_name" ]; then
-    model_name="DeepSeek-R1-Distill-Qwen-7B"
+    model_name=${default_llm_model}
   fi
 
   if [ -z "$version" ]; then
-    version="0.1"
+    version=${default_version}
   fi
 
   typeset -l model_name_lower
   model_name_lower=$model_name
 
-  echo "from [$llm_base_image] build model image with name [$model_name_lower:$version]"
+  echo "from [${llm_base_image}] build llm model image with name [$model_name_lower:$version]"
 
   cd $workdir || exit
   docker_build_dir=$workdir/dockerfiles/llm/model/build
   mkdir -p "$docker_build_dir"
   rm -rf "$docker_build_dir"/*
 
-  cp -r "$workdir"/mis*.whl "$docker_build_dir"
-  cp -r "$workdir"/configs "$docker_build_dir"
+  cp -r "$workdir"/llm/mis*.whl "$docker_build_dir"
   cp -r "$workdir"/patch "$docker_build_dir"
+
+  mkdir -p "$docker_build_dir"/configs/llm/$model_name_lower
+  cp -r "$workdir"/configs/llm/$model_name_lower/* "$docker_build_dir"/configs/llm/$model_name_lower
 
   cd $workdir/dockerfiles/llm/model || exit
 
-  docker build --build-arg BASE_IMAGE=$llm_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
+  docker build --build-arg BASE_IMAGE=${llm_base_image} --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
+}
+
+function build_vlm_model_image() {
+  vlm_base_image=$2
+  model_name=$3
+  version=$4
+
+  if [ -z "${vlm_base_image}" ]; then
+    vlm_base_image=${default_vlm_base}
+  fi
+
+  if [ -z "$model_name" ]; then
+    model_name=${default_vlm_model}
+  fi
+
+  if [ -z "$version" ]; then
+    version=${default_version}
+  fi
+
+  typeset -l model_name_lower
+  model_name_lower=$model_name
+
+  echo "from [${vlm_base_image}] build vlm model image with name [$model_name_lower:$version]"
+
+  cd $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/vlm/model/build
+  mkdir -p "$docker_build_dir"
+  rm -rf "$docker_build_dir"/*
+
+  cp -r "$workdir"/vlm/mis*.whl "$docker_build_dir"
+
+  mkdir -p "$docker_build_dir"/configs/llm/$model_name_lower
+  cp -r "$workdir"/configs/llm/$model_name_lower/* "$docker_build_dir"/configs/llm/$model_name_lower
+
+  cd $workdir/dockerfiles/vlm/model || exit
+
+  docker build --build-arg BASE_IMAGE=${vlm_base_image} --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
 }
 
 stage=$1
 case $stage in
-  "cann")
-    build_cann_image "$1" "$2"
+  "llm-cann")
+    build_llm_cann_image "$1" "$2"
     ;;
   "llm-base")
     build_llm_base_image "$1" "$2" "$3"
     ;;
-  "model")
-    build_model_image "$1" "$2" "$3" "$4"
+  "llm-model")
+    build_llm_model_image "$1" "$2" "$3" "$4"
     ;;
-  *) echo "Invalid stage, we only support [cann, llm-base, model]"
+  "vlm-cann")
+    build_vlm_cann_image "$1" "$2"
+    ;;
+  "vlm-base")
+    build_vlm_base_image "$1" "$2" "$3"
+    ;;
+  "vlm-model")
+    build_vlm_model_image "$1" "$2" "$3" "$4"
+    ;;
+  *) echo "Invalid stage, we only support [llm-cann, llm-base, llm-model, vlm-cann, vlm-base, vlm-model]"
 esac
