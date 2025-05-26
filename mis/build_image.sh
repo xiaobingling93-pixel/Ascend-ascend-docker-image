b45d9f870bf7a4d492b18aa41ac1700a80e6ac72
Jeaniowang <1104133197@qq.com>
2025-05-26 20:04:03 +0800

    修复构建脚本

--- mis/build_image.sh
+++ mis/build_image.sh
@@ -50,6 +50,60 @@ function build_vlm_cann_image() {
   fi
 }
 
+function build_tei_cann_image() {
+  cann_image=$2
+  platform=$3
+  python_version=$4
+  if [ -z "$cann_image" ]; then
+    cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$platform" ]; then
+    platform="910B"
+  fi
+
+  if [ -z "$python_version" ]; then
+    python_version="3.10"
+  fi
+
+  echo "build cann image with name [$cann_image]"
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${cann_image}&"; then
+    echo "cann image: ${cann_image} exist"
+  else
+    echo "building cann image: ${cann_image}"
+    cd "$workdir"/dockerfiles/cann || exit
+    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg PYTHON_VER=${python_version} -t $cann_image . || exit
+  fi
+}
+
+function build_clip_cann_image() {
+  cann_image=$2
+  platform=$3
+  python_version=$4
+  if [ -z "$cann_image" ]; then
+    cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$platform" ]; then
+    platform="910B"
+  fi
+
+  if [ -z "$python_version" ]; then
+    python_version="3.10"
+  fi
+
+  echo "build cann image with name [$cann_image]"
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${cann_image}&"; then
+    echo "cann image: ${cann_image} exist"
+  else
+    echo "building cann image: ${cann_image}"
+    cd "$workdir"/dockerfiles/cann || exit
+    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg PYTHON_VER=${python_version} -t $cann_image . || exit
+  fi
+}
+
 function build_llm_base_image() {
   llm_cann_image=$2
   if [ -z "${llm_cann_image}" ]; then
@@ -91,6 +145,75 @@ function build_vlm_base_image() {
     echo "building vlm-base image: ${vlm_base_image}"
     cd "$workdir"/dockerfiles/vlm/base || exit
     docker build --build-arg BASE_IMAGE=${vlm_cann_image} -t ${vlm_base_image} . || exit
+  fi
+}
+
+
+function build_tei_base_image() {
+  cann_image=$2
+  tei_base_image=$3
+  platform=$4
+
+  if [ -z "$cann_image" ]; then
+    cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$tei_base_image" ]; then
+    tei_base_image="mis-tei-base:0.1"
+  fi
+
+  if [ -z "$platform" ]; then
+    platform="910B"
+  fi
+
+  echo "from [$cann_image] build tei-base image with name [$tei_base_image]"
+
+  cd $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/emb/tei/base/build
+  mkdir -p $docker_build_dir
+  cp -r $workdir/patch/tei.patch $docker_build_dir
+  cd $workdir/dockerfiles/emb/tei/base || exit
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${tei_base_image}&"; then
+    echo "tei-base image: ${tei_base_image} exist"
+  else
+    echo "building tei-base image: ${tei_base_image}"
+    cd "$workdir"/dockerfiles/emb/tei/base || exit
+    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$cann_image --build-arg ARCH=$(uname -m) --build-arg PLATFORM=${platform} -t $tei_base_image . || exit
+  fi
+}
+
+function build_clip_base_image() {
+  cann_image=$2
+  clip_base_image=$3
+  platform=$4
+
+  if [ -z "$cann_image" ]; then
+    cann_image="mis-cann:0.1"
+  fi
+
+  if [ -z "$clip_base_image" ]; then
+    clip_base_image="mis-clip-base:0.1"
+  fi
+
+  if [ -z "$platform" ]; then
+    platform="910B"
+  fi
+
+  echo "from [$cann_image] build clip-base image with name [$clip_base_image]"
+
+  cd $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/emb/clip/base/build
+  mkdir -p $docker_build_dir
+  cp -r $workdir/patch/clip.patch $docker_build_dir
+  cd $workdir/dockerfiles/emb/clip/base || exit
+
+  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${clip_base_image}&"; then
+    echo "clip-base image: ${clip_base_image} exist"
+  else
+    echo "building clip-base image: ${clip_base_image}"
+    cd "$workdir"/dockerfiles/emb/clip/base || exit
+    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$cann_image --build-arg ARCH=$(uname -m) --build-arg PLATFORM=${platform} -t $clip_base_image . || exit
   fi
 }
 
@@ -169,161 +292,6 @@ function build_vlm_model_image() {
   docker build --build-arg BASE_IMAGE=${vlm_base_image} --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
 }
 
-
------------------------------------------------
-function build_cann_image() {
-  cann_image=$2
-  platform=$3
-  python_version=$4
-  if [ -z "$cann_image" ]; then
-    cann_image="mis-cann:0.1"
-  fi
-
-  if [ -z "$platform" ]; then
-    platform="910B"
-  fi
-
-  if [ -z "$python_version" ]; then
-    python_version="3.10"
-  fi
-
-  echo "build cann image with name [$cann_image]"
-
-  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${cann_image}&"; then
-    echo "cann image: ${cann_image} exist"
-  else
-    echo "building cann image: ${cann_image}"
-    cd "$workdir"/dockerfiles/cann || exit
-    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg PYTHON_VER=${python_version} -t $cann_image . || exit
-  fi
-}
-
-function build_llm_base_image() {
-  cann_image=$2
-  if [ -z "$cann_image" ]; then
-    cann_image="mis-cann:0.1"
-  fi
-
-  llm_base_image=$3
-  if [ -z "$llm_base_image" ]; then
-    llm_base_image="mis-llm-base:0.1"
-  fi
-
-  echo "from [$cann_image] build llm-base image with name [$llm_base_image]"
-
-  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${llm_base_image}&"; then
-    echo "llm-base image: ${llm_base_image} exist"
-  else
-    echo "building llm-base image: ${llm_base_image}"
-    cd "$workdir"/dockerfiles/llm/base || exit
-    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg PLATFORM=${platform} --build-arg BASE_IMAGE=$cann_image -t $llm_base_image . || exit
-  fi
-}
-
-
-function build_tei_base_image() {
-  cann_image=$2
-  tei_base_image=$3
-  platform=$4
-
-  if [ -z "$cann_image" ]; then
-    cann_image="mis-cann:0.1"
-  fi
-
-  if [ -z "$tei_base_image" ]; then
-    tei_base_image="mis-tei-base:0.1"
-  fi
-
-  if [ -z "$platform" ]; then
-    platform="910B"
-  fi
-
-  echo "from [$cann_image] build tei-base image with name [$tei_base_image]"
-
-  cd $workdir || exit
-  docker_build_dir=$workdir/dockerfiles/emb/tei/base/build
-  mkdir -p $docker_build_dir
-  cp -r $workdir/patch/tei.patch $docker_build_dir
-  cd $workdir/dockerfiles/emb/tei/base || exit
-
-  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${tei_base_image}&"; then
-    echo "tei-base image: ${tei_base_image} exist"
-  else
-    echo "building tei-base image: ${tei_base_image}"
-    cd "$workdir"/dockerfiles/emb/tei/base || exit
-    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$cann_image --build-arg ARCH=$(uname -m) --build-arg PLATFORM=${platform} -t $tei_base_image . || exit
-  fi
-}
-
-function build_clip_base_image() {
-  cann_image=$2
-  clip_base_image=$3
-  platform=$4
-
-  if [ -z "$cann_image" ]; then
-    cann_image="mis-cann:0.1"
-  fi
-
-  if [ -z "$clip_base_image" ]; then
-    clip_base_image="mis-clip-base:0.1"
-  fi
-
-  if [ -z "$platform" ]; then
-    platform="910B"
-  fi
-
-  echo "from [$cann_image] build clip-base image with name [$clip_base_image]"
-
-  cd $workdir || exit
-  docker_build_dir=$workdir/dockerfiles/emb/clip/base/build
-  mkdir -p $docker_build_dir
-  cp -r $workdir/patch/clip.patch $docker_build_dir
-  cd $workdir/dockerfiles/emb/clip/base || exit
-
-  if docker images --format "{{.Repository}}{{.Tag}}" | grep -q "^${clip_base_image}&"; then
-    echo "clip-base image: ${clip_base_image} exist"
-  else
-    echo "building clip-base image: ${clip_base_image}"
-    cd "$workdir"/dockerfiles/emb/clip/base || exit
-    docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$cann_image --build-arg ARCH=$(uname -m) --build-arg PLATFORM=${platform} -t $clip_base_image . || exit
-  fi
-}
-
-function build_model_image() {
-  llm_base_image=$2
-  model_name=$3
-  version=$4
-
-  if [ -z "$llm_base_image" ]; then
-    llm_base_image="mis-llm-base:0.1"
-  fi
-
-  if [ -z "$model_name" ]; then
-    model_name="DeepSeek-R1-Distill-Qwen-7B"
-  fi
-
-  if [ -z "$version" ]; then
-    version="0.1"
-  fi
-
-  typeset -l model_name_lower
-  model_name_lower=$model_name
-
-  echo "from [$llm_base_image] build model image with name [$model_name_lower:$version]"
-
-  cd $workdir || exit
-  docker_build_dir=$workdir/dockerfiles/llm/model/build
-  mkdir -p "$docker_build_dir"
-  rm -rf "$docker_build_dir"/*
-
-  cp -r "$workdir"/mis*.whl "$docker_build_dir"
-  cp -r "$workdir"/configs "$docker_build_dir"
-  cp -r "$workdir"/patch "$docker_build_dir"
-
-  cd $workdir/dockerfiles/llm/model || exit
-
-  docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$llm_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
-}
 
 function build_tei_model_image() {
   tei_base_image=$2
@@ -404,6 +372,43 @@ function build_clip_model_image() {
   sed -i "s|export MIS_MODEL=.*|export MIS_MODEL=MindSDK/${model_name}|g" $workdir/dockerfiles/emb/tei/model/start_clip.sh
 
   docker build --no-cache --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$clip_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
+}
+
+
+function build_model_image() {
+  llm_base_image=$2
+  model_name=$3
+  version=$4
+
+  if [ -z "$llm_base_image" ]; then
+    llm_base_image="mis-llm-base:0.1"
+  fi
+
+  if [ -z "$model_name" ]; then
+    model_name="DeepSeek-R1-Distill-Qwen-7B"
+  fi
+
+  if [ -z "$version" ]; then
+    version="0.1"
+  fi
+
+  typeset -l model_name_lower
+  model_name_lower=$model_name
+
+  echo "from [$llm_base_image] build model image with name [$model_name_lower:$version]"
+
+  cd $workdir || exit
+  docker_build_dir=$workdir/dockerfiles/llm/model/build
+  mkdir -p "$docker_build_dir"
+  rm -rf "$docker_build_dir"/*
+
+  cp -r "$workdir"/mis*.whl "$docker_build_dir"
+  cp -r "$workdir"/configs "$docker_build_dir"
+  cp -r "$workdir"/patch "$docker_build_dir"
+
+  cd $workdir/dockerfiles/llm/model || exit
+
+  docker build --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTP_PROXY --build-arg BASE_IMAGE=$llm_base_image --build-arg MODEL="$model_name" -t "$model_name_lower":"$version" . || exit
 }
 
 
